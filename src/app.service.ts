e1aac04c38d53e8f6ff466c293fb1f136847a773
hyc <992273994@qq.com>
2019-08-18 14:25:21 +0800

    log

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-	getHello(): string {
+	getHello(@Param('token') token): string {
 		console.log('微信服务器get');
+		console.log('token:', token);
+
 		return 'hello wordl';
 	}
 }
