11d048290001ae1a75fa9f811ecad6d27e7d67ab
hyc <992273994@qq.com>
2019-08-18 15:48:46 +0800

    xml

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Req, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
+var parseString = require('xml2js').parseString;
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
@@ -17,8 +18,10 @@ export class AppController {
 	postMessage(@Body() body, @Req() req) {
 		console.log(body);
 		req.setEncoding('utf8');
-		req.on('data', function(chunk) {
-			console.log('BODY: ' + chunk);
+		req.on('data', function(xml) {
+			parseString(xml, function(err, result) {
+				console.log(result);
+			});
 		});
 
 		console.log('wechat msg posting');
