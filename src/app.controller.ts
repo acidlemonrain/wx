3a7d2d6c5b09d7348ce0f48d8d0b75d01ed6ee97
hyc <992273994@qq.com>
2019-08-18 16:23:30 +0800

    asdads

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Req, Post, Body } from '@nestjs/common';
+import { Controller, Get, Req, Post, Body, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { json } from 'body-parser';
 
 var parseString = require('xml2js').parseString;
 var xml2js = require('xml2js');
-var builder = new xml2js.Builder();
+
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
@@ -18,14 +18,26 @@ export class AppController {
 	}
 
 	@Post()
-	postMessage(@Body() body, @Req() req) {
+	postMessage(@Body() body, @Req() req, @Res() response) {
 		console.log(body);
 		req.setEncoding('utf8');
 		req.on('data', function(xml) {
 			parseString(xml, function(err, result) {
-				console.log(JSON.parse(result));
-				const xml = builder(JSON.parse(result));
-				return xml;
+				console.log(result);
+				var builder = new xml2js.Builder({ cdata: true, rootName: 'xml', headless: true });
+				const wxmsg = {
+					ToUserName: result.xml.FromUserName,
+					FromUserName: result.xml.ToUserName,
+					CreateTime: result.xml.CreateTime,
+					MsgType: 'text',
+					Content: Math.random()
+				};
+				var xml = builder.buildObject(wxmsg);
+				console.log('type', typeof xml);
+				console.log('raw', xml);
+
+				response.set('Content-Type', 'text/xml');
+				response.send(xml);
 			});
 		});
 
