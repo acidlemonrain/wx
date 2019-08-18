3b1ee178df0e5bc30383659c705e1f942feae89b
hyc <992273994@qq.com>
2019-08-18 15:53:05 +0800

    reswebha

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get, Req, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { json } from 'body-parser';
 
 var parseString = require('xml2js').parseString;
+var xml2js = require('xml2js');
+var builder = new xml2js.Builder();
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
@@ -20,7 +23,9 @@ export class AppController {
 		req.setEncoding('utf8');
 		req.on('data', function(xml) {
 			parseString(xml, function(err, result) {
-				console.log(result);
+				console.log(JSON.parse(result));
+				const xml = builder(JSON.parse(result));
+				return xml;
 			});
 		});
 
